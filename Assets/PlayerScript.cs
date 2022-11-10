90de43706ae1ca37ce72e965b0ec7b6257a0d63b
Vaness <vaness13337@gmail.com>
2022-11-10 16:04:47 +0700

    movement-completed

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {   
     public float movementSpeed;
+    public float jumpForce;
     public GameObject characterSprite;
+
+    //State
+    bool jumping = false;
     
 
     // Start is called before the first frame update
@@ -16,6 +20,7 @@ public class PlayerScript : MonoBehaviour
 
     void move()
     {
+        Animation anim = characterSprite.GetComponent<Animation>();
         if (Input.GetKeyDown(KeyCode.A))
         {
             characterSprite.transform.localRotation = new Quaternion(0, 180, 0, 0);
@@ -28,9 +33,35 @@ public class PlayerScript : MonoBehaviour
         gameObject.transform.Translate(newPos);
     }
 
+    void jump()
+    {
+        jumping = true;
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+    }
+
+    void checkJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, Vector2.down, gameObject.transform.localScale.y);
+        GameObject hitObj = hit.transform.gameObject;
+        Debug.DrawRay(gameObject.transform.position, Vector2.down, Color.red, 0.01f, false);
+        Debug.Log(hitObj);
+        if (hitObj.tag == "Ground")
+        {
+            jumping = false;
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         move();
+        checkJump();
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+            jump();
+            Debug.Log("Jump");
+        }
     }
 }
